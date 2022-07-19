340d9dd8eca0ad42a4c0703775d6077dbe70547a
Motorenger <90602764+Motorenger@users.noreply.github.com>
2022-07-20 01:22:58 +0300

    comments

--- main.py
+++ main.py
@@ -1,19 +1,25 @@
 
+# reading every line with generator
 def read_file():
     for line in open("passwords.txt", "r"):
 
         yield line
 
 
+# actually algorithm
 def checker():
+    # result variable
     valid_passwords = 0
+
     for line in read_file():
+        # cleaning line and taking data we need
         line = line.strip().split(" ")
         char, rule, password = line[0], line[1][:-1], line[2]
         rule_min, rule_max = rule.split("-")
 
+        # processing over checkup
         if password.count(char) != 0 and int(rule_max) >= password.count(char) >= int(rule_min):
-            valid_passwords+=1
+            valid_passwords+=1 # increasing over result count
 
     return valid_passwords
 
